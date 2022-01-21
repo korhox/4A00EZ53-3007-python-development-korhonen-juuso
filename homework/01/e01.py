e1878b19b1d58a2f07da08b3ddb078bac9b29a9e
Juuso Korhonen <juuso@korho.fi>
2022-01-21 14:17:38 +0200

    Add e01

--- homework/01/e01.py
+++ homework/01/e01.py
@@ -1,1 +1,11 @@
-print("This is exercise 01")
+import tkinter, turtle
+kilpikonna = turtle.Turtle()
+kilpikonna.shape("turtle")
+
+i = 0
+while i < 4:
+    kilpikonna.forward(50)
+    kilpikonna.left(90)
+    i = i + 1
+
+tkinter.mainloop()
