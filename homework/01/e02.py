90855b1a6ccf53049c71c7b93999179e829f7129
Juuso Korhonen <juuso@korho.fi>
2022-01-21 14:45:34 +0200

    Add 02

--- homework/01/e02.py
+++ homework/01/e02.py
@@ -1,1 +1,28 @@
-print("This is exercise 02")
+import tkinter, turtle
+kilpikonna = turtle.Turtle()
+kilpikonna.shape("turtle")
+
+print("Mihin suuntaan haluat siirtää kilpikonnaa?")
+print("Kirjoita oikea, vasen, ylös tai alas (o, v, y, a)")
+
+while True:
+    suunta = input()
+
+    if suunta == "oikea" or suunta == "o":
+        kilpikonna.setheading(0)
+        kilpikonna.forward(50)
+
+    if suunta == "vasen" or suunta == "v":
+        kilpikonna.setheading(180)
+        kilpikonna.forward(50)
+
+    if suunta == "ylös" or suunta == "y":
+        kilpikonna.setheading(90)
+        kilpikonna.forward(50)
+
+    if suunta == "alas" or suunta == "a":
+        kilpikonna.setheading(270)
+        kilpikonna.forward(50)
+
+
+tkinter.mainloop()
