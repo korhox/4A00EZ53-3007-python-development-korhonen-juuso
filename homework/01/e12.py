fa204efc26b159b40c6dae2ece24fcf74efa668a
Juuso Korhonen <juuso@korho.fi>
2022-01-22 21:47:11 +0200

    Add 12

---
+++ homework/01/e12.py
@@ -0,0 +1,11 @@
+input1 = int(input())
+input2 = int(input())
+
+sum = input1 + input2
+
+if sum == 10:
+    print("The sum of the given numbers is exactly 10")
+elif sum > 10:
+    print("The sum of the given numbers is greather than 10")
+else:
+    print("The sum of the given numbers is less than 10")
