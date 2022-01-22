811b3fa21e47048159b63aa7c3ffa257a339f953
Juuso Korhonen <juuso@korho.fi>
2022-01-22 23:15:09 +0200

    Add e14

---
+++ homework/01/e14.py
@@ -0,0 +1,9 @@
+print("Give a number of day:")
+day = int(input())
+print("Give a number of month:")
+month = int(input())
+
+if day == 24 and month == 12:
+    print("Merry christmas!")
+else:
+    print("It's not christmas yet!")
