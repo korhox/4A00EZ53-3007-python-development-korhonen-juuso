3708f45cd9b6a2bbe2e3074fca3458839d475639
Juuso Korhonen <juuso@korho.fi>
2022-01-22 03:05:18 +0200

    Add e6

---
+++ homework/01/e06.py
@@ -0,0 +1,12 @@
+print("Give a number 1")
+number1 = int(input())
+print("Give a number 2")
+number2 = int(input())
+print("Give a number 3")
+number3 = int(input())
+
+sum = number1 + number2 + number3
+
+product = sum / 3
+
+print("The average of the numbers is: " + str(product))
