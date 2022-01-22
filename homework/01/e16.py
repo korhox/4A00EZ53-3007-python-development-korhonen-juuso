1e342b3cd485733fb743c11b894f6936871a0389
Juuso Korhonen <juuso@korho.fi>
2022-01-22 23:46:37 +0200

    Add e16

---
+++ homework/01/e16.py
@@ -0,0 +1,28 @@
+print("\n\nA, 0-9:")
+i = 0
+while i <= 9:
+    print(i)
+    i = i + 1
+
+print("\n\nB, 1-10:")
+i = 1
+while i <= 10:
+    print(i)
+    i = i + 1
+
+
+print("\n\nC, 10-1:")
+i = 10
+while i >= 1:
+    print(i)
+    i = i - 1
+
+
+print("\n\nC, 1-n:")
+print("Please give n to iterate to:")
+i = 1
+n = int(input())
+while i <= n:
+    print(i)
+    i = i + 1
+
