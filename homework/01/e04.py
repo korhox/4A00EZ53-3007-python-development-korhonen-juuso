dc93900c3c24c434179e35dcc5e1869a6f371bb4
Juuso Korhonen <juuso@korho.fi>
2022-01-22 02:45:18 +0200

    Add e4

---
+++ homework/01/e04.py
@@ -0,0 +1,20 @@
+# Tulosta tietokoneen näytölle teksti "Anna luku 1".
+print("Anna luku 1")
+
+# Ota käyttäjältä luku 1 vastaan ja talleta se muuttujaan luku1.
+luku1 = int( input() )
+
+# Tulosta tietokoneen näytölle teksti "Anna luku 2".
+print("Anna luku 2")
+
+# Ota käyttäjältä luku 1 vastaan ja talleta se muuttujaan luku2.
+luku2 = int( input() )
+
+# Laske luku1 * luku2 ja tallenna tulos muuttujaan tulo.
+tulo = luku1 * luku2
+
+# Tulosta tietokoneen näytölle teksti "Tulo = "
+print("Tulo = ")
+
+# Tulosta tietokoneen näytölle tulo muuttujan sisältö.
+print(tulo)
