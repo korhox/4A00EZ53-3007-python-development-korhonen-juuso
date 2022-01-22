097fa495e918c7b8c352b60066535118bf14a958
Juuso Korhonen <juuso@korho.fi>
2022-01-22 03:13:56 +0200

    add e07

---
+++ homework/01/e07.py
@@ -0,0 +1,12 @@
+print("Anna luku 1")
+luku1 = int(input())
+
+print("Anna luku 2")
+luku2 = int(input())
+
+if (luku1 != 0 and luku2 != 0):
+    jako = luku1 / luku2
+    print(jako)
+else:
+    print("Division by zero is not possible, dumbass.")
+
