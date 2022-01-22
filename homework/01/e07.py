1cecca297368491e832effffe29864dd7fe6ef5d
Juuso Korhonen <juuso@korho.fi>
2022-01-22 03:18:06 +0200

    It seems i did this as ex8, oops

--- homework/01/e07.py
+++ homework/01/e07.py
@@ -4,9 +4,6 @@ luku1 = int(input())
 print("Anna luku 2")
 luku2 = int(input())
 
-if (luku1 != 0 and luku2 != 0):
+if luku2 != 0:
     jako = luku1 / luku2
     print(jako)
-else:
-    print("Division by zero is not possible, dumbass.")
-
