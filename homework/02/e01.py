d337b668e36b17c374b180436ec9a87f7be24620
Juuso Korhonen <juuso@korho.fi>
2022-01-28 13:07:36 +0200

    Add e1

---
+++ homework/02/e01.py
@@ -0,0 +1,14 @@
+print("hello world")
+
+a = "hello"
+b = "world"
+print(a)
+print(a, b)
+print(a, b, sep=":")
+print(a, b, sep=":", end=";\n")
+
+# Add tab and new line as separator
+print(a, b, sep="\t\n")
+
+# Add new line as separator
+print(a, b, sep="\n")
