916159fc3f53a4d0cd3fb409a043eebb6ced61ff
Juuso Korhonen <juuso@korho.fi>
2022-02-01 23:27:54 +0200

    Add e04

---
+++ homework/02/e04.py
@@ -0,0 +1,26 @@
+a = str(True)
+b = str(5)
+
+print(a + b)
+
+x = 1.5
+print(x)
+print(int(x))
+
+x = 3
+print(float(x))
+
+x = 4
+y = 6
+sum = x + y
+print(sum, type(sum))
+
+x = 4
+y = 5.9
+sum = x + y
+print(sum, type(sum))
+
+x = 6
+y = 3
+div = x / y
+print(div, type(sum))
