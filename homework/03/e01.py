25a96aeab2ecf7e1468f01e2c83ca7292d01c4d2
Juuso Korhonen <juuso@korho.fi>
2022-02-04 14:35:56 +0200

    Add e1

---
+++ homework/03/e01.py
@@ -0,0 +1,12 @@
+for x in range(1, 10):
+    print(x, end=", ")
+print(x+1)
+
+for x in range(0, 10):
+    if (x % 2) == 0:
+        print(x, end=", ")
+print(x+1)
+
+for x in range(10, 0, -1):
+    print(x, end=", ")
+print(x-1)
