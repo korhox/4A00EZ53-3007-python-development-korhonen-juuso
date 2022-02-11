c8c82fa06a97b12a463c425549f53b65ba7181ba
Juuso Korhonen <juuso@korho.fi>
2022-02-11 15:08:33 +0200

    Make bit more sensible

--- homework/04/e01.py
+++ homework/04/e01.py
@@ -1,13 +1,12 @@
 import random
 
-randList = [
-    random.randint(1, 40),
-    random.randint(1, 40),
-    random.randint(1, 40),
-    random.randint(1, 40),
-    random.randint(1, 40),
-    random.randint(1, 40),
-    random.randint(1, 40)
-]
+# create set, which does not allow duplicates
+lotto = set()
 
-print(randList)
+# while loop until set is 7 ints long (if added value is same as one added before, it will be not added/it is overriden)
+while len(lotto) < 7:
+    # generate random number between 1 - 40
+    lotto.add(random.randint(1, 40))
+
+# print generated set
+print(lotto)
