105e79374ce55e5cea854ded29623fd9616b22f4
Juuso Korhonen <juuso@korho.fi>
2022-02-25 13:47:20 +0200

    Add e4

--- homework/06/e02-e06/app.py
+++ homework/06/e02-e06/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from datetime import datetime
 import htmlhelper
+import random
 
 # create Flask object, give module name
 # where to look for resources, like templates or static files
@@ -28,6 +29,38 @@ def date():
 @app.route("/date5")
 def date5():
     return htmlhelper.generate_html_page("date", f"<p>{datetime.now()}</p>")
+
+
+@app.route("/slots")
+def slots():
+    images = [
+        "/static/peach.svg",
+        "/static/eggplant.svg",
+        "/static/chili.svg",
+    ]
+
+    slots = []
+    for i in range(3):
+        slots.append(random.randint(0, 2))
+
+    imageHtml = f""
+    win = True
+    last = slots[0]
+    for i in slots:
+        imageHtml += f"<img src='{images[i]}' width='40' height='40'>"
+        if last != i:
+            win = False
+        last = i
+
+    if win:
+        winHtml = "<p class='text-center text-success'>Congrats! You won the slot game!</p>"
+    else:
+        winHtml = "<p class='text-center text-danger'>No win.</p>"
+
+    return htmlhelper.generate_html_page("date", f"""
+        <p class='text-center mt-5'>{imageHtml}</p>
+        {winHtml}
+        """.strip())
 
 
 # start the app if using python3 app.py
--- homework/06/e02-e06/htmlhelper.py
+++ homework/06/e02-e06/htmlhelper.py
@@ -8,12 +8,18 @@ def generate_html_page(title, content):
     <head>
         <meta charset="utf-8">
         <title>{title}</title>
+        <link rel="stylesheet" href="https://cdn.bittivirta.fi/packages/bootstrap/v5.1.3/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdn.bittivirta.fi/css/fonts/font-package.css">
+        <style>
+            body {{
+                font-family:'Ubuntu';
+            }}
+            code {{
+                font-family:'Fira Code Nerd';
+            }}
+        </style>
     </head>
     <body>
         {content}
     </body>
 </html>"""
-
-
-html_page = generate_html_page("My title", "<p>Hello</p>")
-print(html_page)
