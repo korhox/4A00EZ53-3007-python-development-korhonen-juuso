4ab93b87d3ab3e074ec8bb5cb341d30050d241ec
korhox <juuso@korho.fi>
2022-06-09 00:08:25 +0300

    feat: add reset and remove functions & fix add word default word list

--- project/backend/src/filer.py
+++ project/backend/src/filer.py
@@ -37,13 +37,30 @@ class Filer:
         return True
 
     @staticmethod
+    def reset_words():
+        path = Path("storage/words.txt")
+        if path.exists() != True:
+            os.makedirs(os.path.dirname(path.absolute()), exist_ok=True)
+        f = open(path.absolute(), "w+")
+        f.write(Filer.default_wordlist)
+        f.close()
+        return True
+
+    @staticmethod
     def add_word(word):
         if validator.Validator.validate_word(word) == True:
-            words = default_wordlist
+            words = Filer.get_all_words()
             words.append(word)
             Filer.save_wordlist(words)
         else:
             raise "Word did not pass the validation, please use only 2-30 a-z letters"
+
+    @staticmethod
+    def remove_word(index):
+        words = Filer.get_all_words()
+        words.pop(index)
+        Filer.save_wordlist(words)
+        return True
 
     @staticmethod
     def word_exists(word):
