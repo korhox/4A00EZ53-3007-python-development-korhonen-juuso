3aca6bae02bdfc508f0d612b9ade59f490c7ee76
korhox <juuso@korho.fi>
2022-06-16 20:20:45 +0300

    feat: add possibility to save scores and fix few bugs

--- project/backend/src/scorer.py
+++ project/backend/src/scorer.py
@@ -4,13 +4,6 @@ import os
 from simple_chalk import *
 from pathlib import Path
 
-score = 0
-
-
-def add(points: int):
-    score += points
-    return True
-
 
 def get_scores(word: str):
     if filer.word_exists(word) != True:
@@ -18,14 +11,38 @@ def get_scores(word: str):
         return False
     else:
         path = Path("storage/scores/" + word + ".txt")
-        if path.exists() != True:
-            os.makedirs(os.path.dirname(path.absolute()), exist_ok=True)
-            f = open(path.absolute(), "w+")
-            f.write("-:0;-:0;-:0;")
-            f.close()
-        scores = filer.read(path).split(";")
-        i = 0
-        while i < len(scores):
+        scores = filer.read(path)
+        if scores == "":
+            return []
+        scores = scores.split("\n")
+        for i in range(len(scores)):
             scores[i] = scores[i].split(":")
-            i += 1
         return scores
+
+
+def reset():
+    words = filer.get_all_words()
+    for word in words:
+        try:
+            path = Path("storage/scores/" + word + ".txt")
+            file = open(path.absolute(), "w")
+            file.close()
+        except:
+            continue
+    return True
+
+
+def save(name: str, word: str, score: float):
+    path = Path("storage/scores/" + word + ".txt")
+    if path.exists() != True:
+        os.makedirs(os.path.dirname(path.absolute()), exist_ok=True)
+    scores = get_scores(word)
+    scores.append([name, str(score)])
+    scores.sort(key=__sortsecond)
+    file = open(path.absolute(), "w")
+    file.write("\n".join([":".join(x) for x in scores[:3]]))
+    file.close()
+
+
+def __sortsecond(val: list):
+    return float(val[1])
