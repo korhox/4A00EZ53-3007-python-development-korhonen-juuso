8226ed93dfd1dfa82d70d58b77a8ec1cbf397384
korhox <juuso@korho.fi>
2022-06-16 21:18:39 +0300

    Feat: add pydoc comments

--- project/backend/src/main.py
+++ project/backend/src/main.py
@@ -1,7 +1,15 @@
 from game import Game
 
 
+"""
+Main module boots up the game loop
+"""
+
+
 def main():
+    """
+        The main function creates the game object and boots up the game welcome screen
+    """
     game = Game()
     game.welcome()
 
