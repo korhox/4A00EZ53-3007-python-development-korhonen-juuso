78338fc6cc8cadfb72526a7ab1d289374e842913
korhox <juuso@korho.fi>
2022-06-16 20:21:56 +0300

    rebase: remove class

--- project/backend/src/validator.py
+++ project/backend/src/validator.py
@@ -1,20 +1,16 @@
 import re
 
 
-class ValidatorError(Exception):
-    """Cli error handler"""
+def validate_name(name: str):
+    regex = "\w{2,10}"  # allows classic arcade type names using general alphabet, numbers and underscore
+    return bool(re.fullmatch(regex, name))
 
 
-class Validator:
-    @staticmethod
-    def validate_name(name: str):
-        regex = "\w{1,10}"  # allows classic arcade type names using general alphabet, numbers and underscore
-        return bool(re.fullmatch(regex, name))
+def validate_word(word: str):
+    regex = "[a-z]{2,30}"  # allows only english alphabet
+    return bool(re.fullmatch(regex, word))
 
-    def validate_word(word: str):
-        regex = "[a-z]{2,30}"  # allows only english alphabet
-        return bool(re.fullmatch(regex, word))
 
-    def validate_word_letter(letter: str):
-        regex = "[a-z]{1}"  # allows only english alphabet
-        return bool(e.fullmatch(regex, letter))
+def validate_word_letter(letter: str):
+    regex = "[a-z]{1}"  # allows only english alphabet
+    return bool(e.fullmatch(regex, letter))
