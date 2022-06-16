8de129306bc3d8d9f44d975d12cade027ef063eb
korhox <juuso@korho.fi>
2022-06-16 20:21:24 +0300

    rebase: make as class again for better management

--- project/backend/src/timer.py
+++ project/backend/src/timer.py
@@ -1,46 +1,40 @@
 import time
 
-start_time = 0
 
+class Timer:
+    def __init__(self):
+        self.start_time = 0
 
-def start():
-    if start_time != 0:
-        raise Exception("Timer already running")
-        return False
-    else:
-        start_time = float(time.time())
+    def start(self):
+        if self.start_time != 0:
+            raise Exception("Timer already running")
+            return False
+        else:
+            self.start_time = float(time.time())
+            return True
+
+    def get_start(self):
+        if self.start_time == 0:
+            raise Exception("Timer not running")
+            return False
+        else:
+            return self.start_time
+
+    def get(self):
+        if self.start_time == 0:
+            raise Exception("Timer not running")
+            return False
+        else:
+            return float(time.time()) - self.start_time
+
+    def reset(self):
+        self.start_time = 0
         return True
 
-
-def get_start():
-    if start_time == 0:
-        raise Exception("Timer not running")
-        return False
-    else:
-        return start_time
-
-
-def get():
-    if start_time == 0:
-        raise Exception("Timer not running")
-        return False
-    else:
-        return float(time.time()) - start_time
-
-
-def reset():
-    if start_time == 0:
-        raise Exception("Timer not running")
-        return False
-    else:
-        start_time = float(time.time())
-        return True
-
-
-def stop():
-    if start_time == 0:
-        raise Exception("Timer not running")
-        return False
-    else:
-        start_time == 0
-        return True
+    def stop(self):
+        if start_time == 0:
+            raise Exception("Timer not running")
+            return False
+        else:
+            self.start_time == 0
+            return True
