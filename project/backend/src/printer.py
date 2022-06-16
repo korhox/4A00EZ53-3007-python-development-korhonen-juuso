3c7f51352ac09e4f414b4d536bc9fda591f66ce8
korhox <juuso@korho.fi>
2022-06-16 20:19:57 +0300

    feat: add score printer

--- project/backend/src/printer.py
+++ project/backend/src/printer.py
@@ -1,19 +1,43 @@
+import filer
 import cli
+import scorer
 from simple_chalk import *
 
 
 def scoreboard():
+    cli.clear()
     print(cli.create_header("Scoreboard"))
 
-    print(bold("Word                  High Score       Scorer"))
-    for x in filer.get_all_words():
-        highscore = scorer.get_scores(x)[0]
-        line = x.capitalize()
+    print(bold("#    Word                  High Score       Scorer"))
+    words = filer.get_all_words()
+    for i, x in enumerate(words):
+        highscore = (scorer.get_scores(x) or [["-", "-"]])[0]
+        line = str(i+1) + "."
+        line += " " * (4 - len(str(i)))
+        line += x.capitalize()
         line += " " * (22 - len(x))
         line += highscore[1]
         line += " " * (17 - len(highscore[1]))
         line += highscore[0]
         print(line)
+    result = input(cli.format_input("Give word number to see more scores or press Enter to return main menu", 0, "a number / ↩"))
+    if result == "":
+        return
+    if result.isnumeric() and int(result) <= len(words):
+        print_score(words[int(result)-1])
+
+
+def print_score(word: str):
+    cli.clear()
+    scores = scorer.get_scores(word)
+    print(cli.create_header("Scoreboard for word: " + word))
+    print(bold("Scorer                  High Score"))
+    for score in scores:
+        line = score[0]
+        line += " " * (24 - len(score[0]))
+        line += score[1]
+        print(line)
+    input(cli.format_input())
 
 
 def info():
@@ -40,7 +64,7 @@ def welcome_text():
     print(cli.create_header("Welcome!"))
     print("Please adjust the screen / window to fit the following box + text above and below:")
     print("┏" + "━━" * 48 + "┓")
-    for x in range(0, 25):
+    for x in range(0, 21):
         print("┃" + 48 * "  " + "┃")
     print("┗" + "━━" * 48 + "┛")
 
