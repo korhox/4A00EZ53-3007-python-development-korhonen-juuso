b065575c25ef6fca1a3bbc3e1067b25410823f68
korhox <juuso@korho.fi>
2022-06-16 20:14:31 +0300

    fix: organize better

--- project/backend/src/art.py
+++ project/backend/src/art.py
@@ -186,42 +186,22 @@ stages = ["""###################
 # #/ /        ||
 # # /         ||.-''.
 # #/          |/  _  \\
-# #           ||  x/x|
-# #           (\◝_ .'
-# #          .-`--'.
-# #         /Y . . Y\\
-# #        // |   | \\\\
-# #       //  | . |  \\\\
-# #      ')   |   |   (`
-# #           ||-||
-# #    _      || ||
-# #   //      || ||
+# #           ||  x/x|            """+red("""   ██▓     ▒█████    ██████ ▓█████   """)+"""
+# #           (\◝_ .'             """+red("""  ▓██▒    ▒██▒  ██▒▒██    ▒ ▓█   ▀   """)+"""
+# #          .-`--'.              """+red("""  ▒██░    ▒██░  ██▒░ ▓██▄   ▒███     """)+"""
+# #         /Y . . Y\\            """+red("""  ▒██░    ▒██   ██░  ▒   ██▒▒▓█  ▄   """)+"""
+# #        // |   | \\\\          """+red("""  ░██████▒░ ████▓▒░▒██████▒▒░▒████▒  """)+"""
+# #       //  | . |  \\\\         """+red("""  ░ ▒░▓  ░░ ▒░▒░▒░ ▒ ▒▓▒ ▒ ░░░ ▒░ ░  """)+"""
+# #      ')   |   |   (`          """+red("""  ░ ░ ▒  ░  ░ ▒ ▒░ ░ ░▒  ░ ░ ░ ░  ░  """)+"""
+# #           ||-||               """+red("""    ░ ░   ░ ░ ░ ▒  ░  ░  ░     ░     """)+"""
+# #    _      || ||               """+red("""      ░  ░    ░ ░        ░     ░  ░  """)+"""
+# #   //      || ||                 
 # #  //       || ||
 ############  /| |\   ########
 ############\         ########
 # #         \\\\             # #
 : :          \\\\            : :
 . .           `'           . ."""]
-
-lose = """▓██   ██▓ ▒█████   █    ██                   
- ▒██  ██▒▒██▒  ██▒ ██  ▓██▒                  
-  ▒██ ██░▒██░  ██▒▓██  ▒██░                  
-  ░ ▐██▓░▒██   ██░▓▓█  ░██░                  
-  ░ ██▒▓░░ ████▓▒░▒▒█████▓                   
-   ██▒▒▒ ░ ▒░▒░▒░ ░▒▓▒ ▒ ▒                   
- ▓██ ░▒░   ░ ▒ ▒░ ░░▒░ ░ ░                   
- ▒ ▒ ░░  ░ ░ ░ ▒   ░░░ ░ ░                   
- ░ ░         ░ ░     ░                       
- ░ ░                                         
-             ██▓     ▒█████    ██████ ▓█████ 
-            ▓██▒    ▒██▒  ██▒▒██    ▒ ▓█   ▀ 
-            ▒██░    ▒██░  ██▒░ ▓██▄   ▒███   
-            ▒██░    ▒██   ██░  ▒   ██▒▒▓█  ▄ 
-            ░██████▒░ ████▓▒░▒██████▒▒░▒████▒
-            ░ ▒░▓  ░░ ▒░▒░▒░ ▒ ▒▓▒ ▒ ░░░ ▒░ ░
-            ░ ░ ▒  ░  ░ ▒ ▒░ ░ ░▒  ░ ░ ░ ░  ░
-              ░ ░   ░ ░ ░ ▒  ░  ░  ░     ░   
-                ░  ░    ░ ░        ░     ░  ░"""
 
 win = """ooooo  oooo ooooooo  ooooo  oooo                        
   888  88 o888   888o 888    88                         
@@ -239,23 +219,19 @@ win = """ooooo  oooo ooooooo  ooooo  oooo
 def get_stage(stage: int, game):
 
     if stage < 0 or stage > 7:
-        raise Exception("Stage must be between 0 and 8")
+        raise Exception("Stage must be between 0 and 7")
         return False
 
     stageart = stages[:][stage].split("\n")
 
-    stageart[3] += ((34 - len(stageart[3])) * " ") + bold("Guess the word:")
-    stageart[4] += ((34 - len(stageart[4])) * " ") + blue("".join([f"{i} " for i in game.guess]))
+    stageart[1] += ((34 - len(stageart[1])) * " ") + bold("Guess the word:")
+    stageart[2] += ((34 - len(stageart[2])) * " ") + blue("".join([f"{i} " for i in game.guess]))
 
-    stageart[6] += ((34 - len(stageart[6])) * " ") + gray("Word length: ") + str(len(game.guess))
-    stageart[7] += ((34 - len(stageart[7])) * " ") + gray("Wrong guesses: ") + str(game.wrong_guesses)
-    stageart[8] += ((34 - len(stageart[8])) * " ") + "".join([f"{i} " for i in game.guessed_letters])
+    stageart[4] += ((34 - len(stageart[4])) * " ") + gray("Word length: ") + str(len(game.guess))
+    stageart[5] += ((34 - len(stageart[5])) * " ") + gray("Wrong guesses: ") + str(game.wrong_guesses) + gray("/") + str(game.guesses)
+    stageart[6] += ((34 - len(stageart[6])) * " ") + "".join([f"{i} " for i in game.guessed_letters])
 
     return "\n".join(stageart)
-
-
-def get_lose():
-    return red(lose)
 
 
 def get_win():
