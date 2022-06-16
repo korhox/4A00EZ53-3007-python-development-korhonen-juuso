a3e39a31c5d644b5a895a72c3be116b64f73668e
korhox <juuso@korho.fi>
2022-06-16 23:49:45 +0300

    Feat: add pydoc comments

--- project/backend/src/cli.py
+++ project/backend/src/cli.py
@@ -3,10 +3,19 @@ import os
 
 
 def clear():
+    """Clears the CLI"""
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
 def create_header(title: str):
+    """Creates beautiful header with ascii game name and title
+
+    Args:
+        title (string): the title to be added in to the header
+
+    Returns:
+        string: the header
+    """
     header = green.bold(""",-_/,.             ,-,-,-.\n' |_|/ ,-. ,-. ,-. `,| | |   ,-. ,-.\n /| |  ,-| | | | |   | ; | . ,-| | |\n `' `' `-^ ' ' `-|   '   `-' `-^ ' '\n                ,|'  """)
     header += blue(title)
     header += green.bold("""\n                `\n\n""")
@@ -14,6 +23,14 @@ def create_header(title: str):
 
 
 def create_menu(items: list):
+    """creates menu from a list of titles
+
+    Args:
+        items (list): list of strings, each string is one menu item
+
+    Returns:
+        string: _description_
+    """
     menu = ""
     i = 0
     while i < len(items):
@@ -23,10 +40,24 @@ def create_menu(items: list):
 
 
 def format_input(title: str = "Press enter to return main menu", options: int = 0, subtitle: str = "↩"):
-    if title == None:
-        title
-    if subtitle == None:
-        subtitle = "Press enter to return main menu"
+    """Formats input
+
+    The default subtitle is different depending on amount of options. Subtitle parameter overrides this behavior.
+        if options == 0: subtitle = "↩"
+        if options == 1: subtitle = "y/n"
+        if options == 3: subtitle = "1 / 2"
+        if options == 4: subtitle = "1 / 2 / 3 / 4"
+        if options == 8: subtitle = "1 / 2 / 3 / 4 / 5 / 6 / 8"
+        if options >= 2: subtitle = "1 / 2 ..."
+
+    Args:
+        title (str, optional): The title to be shown in question Defaults to "Press enter to return main menu".
+        options (int, optional): How many options you have. Defaults to 0.
+        subtitle (str, optional): Overrides default subtitle which depends on amount of options. Defaults to "↩".
+
+    Returns:
+        _type_: _description_
+    """
     question = "\n" + blue.bold("» ") + white.bold(title) + gray(" (")
     if subtitle != "↩":
         question += gray(subtitle)
